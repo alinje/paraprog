eb55d3d196f610549968a64382de3ea33c1488cc
Aline <aline.eikeland@gmail.com>
2021-03-03 15:01:31 +0100

    Simple attempt at parallel execution
    At this time, does not improve performance much

--- amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
+++ amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
@@ -6,8 +6,14 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
 import java.util.concurrent.ConcurrentSkipListSet;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 /**
  * <code>ForkJoinSolver</code> implements a solver for
@@ -22,6 +28,8 @@ import java.util.concurrent.ConcurrentSkipListSet;
 public class ForkJoinSolver
     extends SequentialSolver
 {
+    int player;
+
     /**
      * Creates a solver that searches in <code>maze</code> from the
      * start node to a goal.
@@ -31,6 +39,26 @@ public class ForkJoinSolver
     public ForkJoinSolver(Maze maze)
     {
         super(maze);
+        visited = new ConcurrentSkipListSet<>();
+        player = maze.newPlayer(maze.start());
+    }
+
+    public ForkJoinSolver(Maze maze, Object start, Set<Integer> visited, Map<Integer, Integer> predecessor) {
+        super(maze);
+
+        this.start = (int)start; //TODO nah
+        this.visited = visited;
+
+
+        player = maze.newPlayer((int)start);
+        this.predecessor = predecessor;
+
+
+    }
+
+    @Override
+    protected void initStructures() {
+        predecessor = new HashMap<>();
     }
 
     /**
@@ -64,11 +92,77 @@ public class ForkJoinSolver
     @Override
     public List<Integer> compute()
     {
-        return parallelSearch();
+        // we do not want this in our recursive call?
+        return parallelSearch(start);
     }
 
-    private List<Integer> parallelSearch()
+    private List<Integer> parallelSearch(int startID)
     {
+        // set visited
+        // map predecessor - maps key node to value node which it came from
+        // thread-safe sets: ConcurrentSkipListSet
+        // a depth first search with forking at each maze fork
+
+        // ConcurrentSkipListSet´s add adds current atomically if not already present, and returns boolean telling if it succeeded
+        if (visited.add(startID)){
+
+            int current = startID;
+            if (maze.hasGoal(current)){
+                
+                maze.move(player, current);
+
+                //TODO do we need to tell the other threads that we are done?
+                
+                return pathFromTo(maze.start(), current);
+            }
+
+
+            maze.move(player, current);
+
+            TreeSet<Integer> neighbours = new TreeSet<>(maze.neighbors(current));
+            Iterator<Integer> it = neighbours.iterator();
+            
+            // remove the already visited nodes from the set
+            while (it.hasNext()){
+                int nxt = it.next();
+                if (visited.contains(nxt)){
+                    it.remove();
+                } else {
+                    predecessor.put(nxt, current);
+                }
+            }
+
+            if (neighbours.size() == 1){
+                return parallelSearch(neighbours.first());
+            } else if (neighbours.size() == 2){ //TODO possibly add condition around forkAfter()
+                ForkJoinSolver breakOut = new ForkJoinSolver(maze, neighbours.pollFirst(), visited, new HashMap<>(predecessor)); // lowest entries to forks because less likely to be right?
+                breakOut.fork();
+                
+                List<Integer> remainsResult = parallelSearch(neighbours.pollLast());
+                
+                if (remainsResult != null) return remainsResult;
+
+                List<Integer> breakOutResults = breakOut.join();
+                if ( breakOutResults != null) return breakOutResults; //TODO this could be done with the questionmarkthingy
+
+            } else if (neighbours.size() == 3){ // neighbours sixe is three
+                ForkJoinSolver breakOut1 = new ForkJoinSolver(maze, neighbours.pollFirst(), visited, new HashMap<>(predecessor));
+                ForkJoinSolver breakOut2 = new ForkJoinSolver(maze, neighbours.pollFirst(), visited, new HashMap<>(predecessor));
+
+                breakOut1.fork();
+                breakOut2.fork();
+
+                List<Integer> remains = parallelSearch(neighbours.pollLast());
+                
+                if (remains != null) return remains;
+
+                List<Integer> breakOutResults = breakOut2.join();
+                if ( breakOutResults != null) return breakOutResults;
+                breakOutResults = breakOut1.join();
+                if ( breakOutResults != null) return breakOutResults;
+            } 
+        }
+
         return null;
     }
 }
