d6600b7724d242e0b75f6baf4cf316da474a87e6
Aline <aline.eikeland@gmail.com>
2021-03-11 13:41:12 +0100

    Cleanup, commenting, map adjustments

--- amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
+++ amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
@@ -30,7 +30,7 @@ public class ForkJoinSolver
     extends SequentialSolver
 {
     private int player;
-    private AtomicBoolean playerSet;
+    private boolean playerSet;
     private AtomicBoolean found;
 
     /**
@@ -44,21 +44,19 @@ public class ForkJoinSolver
         super(maze);
         visited = new ConcurrentSkipListSet<>();
         found = new AtomicBoolean(); // defaults to false
-        playerSet = new AtomicBoolean();
+        playerSet = false;
 
     }
 
     public ForkJoinSolver(Maze maze, Object start, Set<Integer> visited, Map<Integer, Integer> predecessor, AtomicBoolean found) {
         super(maze);
 
-        this.start = (int)start; //TODO nah
+        this.start = (int)start; 
         this.visited = visited;
         this.found = found;
 
-
         this.predecessor = predecessor;
-
-        playerSet = new AtomicBoolean();
+        playerSet = false;
 
     }
 
@@ -102,9 +100,7 @@ public class ForkJoinSolver
         return parallelSearch(start);
     }
 
-
-    //TODO solution should work with wacko forkAfter-values, be optimized with good values
-    private List<Integer> parallelSearch(int startID)
+    private List<Integer> parallelSearch(int current)
     {
         // set visited
         // map predecessor - maps key node to value node which it came from
@@ -112,57 +108,64 @@ public class ForkJoinSolver
         // a depth first search with forking at each maze fork
 
         // ConcurrentSkipListSet´s add adds current atomically if not already present, and returns boolean telling if it succeeded
-        if (visited.add(startID) && !found.get()){
+        if (!visited.add(current) && found.get()){
+            return null;
+        }
 
-            if (!playerSet.get()) {
-                player = maze.newPlayer((int)start);
-                playerSet.set(true);
-            } //TODO this should be changed to a regular boolean
-            int current = startID;
-            maze.move(player, current);
+        // If the player is not yet placed on the map, this is where we set it
+        if (!playerSet) {
+            player = maze.newPlayer((int)start);
+            playerSet = true;
+        } 
+        
+        // We move the player in the GUI
+        maze.move(player, current);
 
-            if (maze.hasGoal(current)){
+        // If the current node is the goal, set found flag to true and return the path to the goal
+        if (maze.hasGoal(current)){
+            found.set(true);
+            return pathFromTo(maze.start(), current);
+        }
 
-                System.out.println(this.getPool().getActiveThreadCount());
-
-                found.set(true);
-                return pathFromTo(maze.start(), current);
-            }
-
-            TreeSet<Integer> neighbours = new TreeSet<>(maze.neighbors(current));
-            Iterator<Integer> it = neighbours.iterator();
-            
-            // remove the already visited nodes from the set
-            while (it.hasNext()){
-                int nxt = it.next();
-                if (visited.contains(nxt)){
-                    it.remove();
-                } else {
-                    predecessor.put(nxt, current);
-                }
-            }
-
-            if(neighbours.isEmpty()) return null;
-
-            List<ForkJoinSolver> children = new ArrayList<>();
-            while (neighbours.size() > 1){
-
-                ForkJoinSolver breakOut = new ForkJoinSolver(maze, neighbours.pollFirst(), visited, new HashMap<>(predecessor), found); // lowest entries to forks because less likely to be right?
-                children.add(breakOut);
-                breakOut.fork();
-            }
-
-
-            List<Integer> remainsResult = parallelSearch(neighbours.pollFirst());            
-            if (remainsResult != null) return remainsResult;
-
-
-            for (ForkJoinSolver child : children) {
-
-                List<Integer> results = child.join();
-                if(results != null) return results;
+        // We get the neighbours of the current node
+        TreeSet<Integer> neighbours = new TreeSet<>(maze.neighbors(current));
+        Iterator<Integer> it = neighbours.iterator();
+        
+        // remove the already visited nodes from the set
+        while (it.hasNext()){
+            int nxt = it.next();
+            if (visited.contains(nxt)){
+                it.remove();
+            } else {
+                predecessor.put(nxt, current);
             }
         }
+
+        // If there are no neighbours, do nothing and return null
+        if(neighbours.isEmpty()) {
+            return null;
+        } 
+
+        List<ForkJoinSolver> children = new ArrayList<>();
+
+        // While there are unvisited neighbours, fork new processes that explore each available path
+        while (neighbours.size() > 0){
+            ForkJoinSolver breakOut = new ForkJoinSolver(maze, neighbours.pollFirst(), visited, new HashMap<>(predecessor), found); // lowest entries to forks because less likely to be right?
+            children.add(breakOut);
+            breakOut.fork();
+        }
+
+        // This is work to do for the mother thread. Not essential since it is a forkjoinPOOL, but saves one forking operation.
+        List<Integer> remainsResult = parallelSearch(neighbours.pollFirst());            
+        if (remainsResult != null) return remainsResult;
+
+        // Join threads when processes finnish
+        for (ForkJoinSolver child : children) {
+            List<Integer> results = child.join();
+            if(results != null) return results;
+        }
+        
+        // If neither this thread nor its children found a path, return null
         return null;
     }
 }
